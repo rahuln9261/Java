1afcd1555c93344992521df8a35d9855ec1062a4
Rahul Nayak <rahul.22mic7152@vitapstudent.ac.in>
2023-02-20 17:09:20 +0530

    Fourth Java Project

---
+++ Greet Hello/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Enter Your Name:");
+        String a=Sc.next();
+        System.out.printf("Hello %s\n",a);
+        System.out.println("Have A Nice Day :D");
+    }
+}
